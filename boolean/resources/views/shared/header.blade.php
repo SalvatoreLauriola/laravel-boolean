4185c9af7d8c0a50e3fd4186a01b57cff4539689
Salvatore Lauriola <salvatore6324@gmail.com>
2020-06-12 18:35:08 +0200

    first part of styling

--- boolean/resources/views/shared/header.blade.php
+++ boolean/resources/views/shared/header.blade.php
@@ -5,16 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Boolean</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+
+    
     
     <header class="main-header">
         <nav class="navbar">
             <a href="{{ route('static-pages.home') }}" class="navbar-brand">Boolean</a>
             <ul>
-                <li><a href="{{ route('static-pages.home') }}">Home</a></li>
+                {{-- Metodo request rende attivo il menu cliccato --}}
+                <li><a @if (Request::route()->getName() == 'static-pages.home') class="active" @endif
+                href="{{ route('static-pages.home') }}">Home</a></li>
                 <li><a href="#">Corso</a></li>
-                <li><a href="{{ route('student.index') }}">Dopo il corso</a></li>
+                
+                <li><a @if (Request::route()->getName() == 'student,index') class="active" @endif
+                href="{{ route('student.index') }}">Dopo il corso</a></li>
                 <li><a href="#">Lezione gratuita</a></li>
                 <li><a href="#">Candidature</a></li>
             </ul>
--- boolean/resources/views/static-pages/index.blade.php
+++ boolean/resources/views/static-pages/index.blade.php
@@ -3,5 +3,17 @@
 @section('main-content')
 
 
-    <h1>HOMEPAGE</h1>
+    <div class="jumbo">
+        <div class="jumbo-text">
+            <h2>Diventa
+                sviluppatore web</h2>
+                <p>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</p>
+                <p>6 mesi di corso intensivo online in diretta
+                    Nessuna competenza di programmazione richiesta
+                    Siamo certi del tuo successo, altrimenti ti rimborsiamo</p>
+        </div>
+        <div class="jumbo-img">
+            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="Impara da casa tua">
+        </div>
+    </div>
 @endsection
